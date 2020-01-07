14c3583525a371278ec532863886b95fceac985d
Fahim Akber <fahimakber03@gmail.com>
2020-01-07 15:16:05 +0600

    task05

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AssignmentTask05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstValue, secondValue, result;
+            Console.WriteLine("Enter 1st Value: ");
+            firstValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd Value: ");
+            secondValue = Convert.ToInt32(Console.ReadLine());
+
+            if(firstValue != 0 && secondValue != 0){
+                result = firstValue / secondValue;
+                Console.WriteLine("your result is : "+result);
+            }
+        }
+    }
+}
